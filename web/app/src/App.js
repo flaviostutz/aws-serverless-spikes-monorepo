737d361255b68481db9ea597682e75945e65c4b6
flaviostutz <flaviostutz@gmail.com>
2022-08-15 23:05:30 +0200

    adding message to app

--- web/app/src/App.js
+++ web/app/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3>We'll have an app soon!</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
