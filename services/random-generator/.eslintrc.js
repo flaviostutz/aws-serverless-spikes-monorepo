3f2a3295d037895ae7cfef15aadd5fe53a359580
flaviostutz <flaviostutz@gmail.com>
2022-08-16 00:44:44 +0200

    updating lint config

--- services/random-generator/.eslintrc.js
+++ services/random-generator/.eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
-    "root": true,
-    "tsconfigRootDir": ".",
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": { "project": ["./tsconfig.json"] },
-    "plugins": [
-        "@typescript-eslint"
-    ]
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  rules: {
+  }
 }
