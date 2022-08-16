71b0c1e03967f76e8d1e56b448a61d3801d428b6
flaviostutz <flaviostutz@gmail.com>
2022-08-16 18:39:18 +0200

    adding another generator endpoint

--- services/random-generator/src/handlers/random-user/handler.ts
+++ services/random-generator/src/handlers/random-user/handler.ts
@@ -2,13 +2,16 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import middy from '@middy/core';
 import jsonBodyParser from '@middy/http-json-body-parser';
 import httpHeaderNormalizer from '@middy/http-header-normalizer';
+import axios from 'axios';
 
 async function lambdaHandler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
   console.log(event.body);
-  const number = Math.round(Math.random() * 99999);
+
+  const resp = await axios.get('https://randomuser.me/api/');
+
   return {
     statusCode: 200,
-    body: `${number}`,
+    body: JSON.stringify(resp.data),
   };
 }
 
