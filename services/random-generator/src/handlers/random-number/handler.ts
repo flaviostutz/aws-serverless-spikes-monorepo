2fe268de75afba289405f40c52ecb476f3f6332d
flaviostutz <flaviostutz@gmail.com>
2022-08-28 18:07:22 +0200

    config cors for services

--- services/random-generator/src/handlers/random-number/handler.ts
+++ services/random-generator/src/handlers/random-number/handler.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import middy from '@middy/core';
+import cors from '@middy/http-cors'
 import jsonBodyParser from '@middy/http-json-body-parser';
 import httpHeaderNormalizer from '@middy/http-header-normalizer';
 
@@ -15,5 +16,6 @@ async function lambdaHandler(event: APIGatewayProxyEvent): Promise<APIGatewayPro
 
 export const handler = middy(lambdaHandler)
   .use(jsonBodyParser())
-  .use(httpHeaderNormalizer());
+  .use(httpHeaderNormalizer())
+  .use(cors());
 
