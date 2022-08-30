e1fc84f86e0ea66769eb70da3f1b5b59f799c9c4
flaviostutz <flaviostutz@gmail.com>
2022-08-30 16:04:07 +0200

    testing canary

--- services/random-generator/src/handlers/random-number/handler.ts
+++ services/random-generator/src/handlers/random-number/handler.ts
@@ -6,11 +6,11 @@ import httpHeaderNormalizer from '@middy/http-header-normalizer';
 
 async function lambdaHandler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
   console.log(event.body);
-  const number = Math.round(Math.random() * 99999);
+  // const number = Math.round(Math.random() * 99999);
   return {
     statusCode: 200,
     body: JSON.stringify({
-      number: `${number}`,
+      number: `999999`,
       timestamp: new Date().toISOString(),
     }),
   };
